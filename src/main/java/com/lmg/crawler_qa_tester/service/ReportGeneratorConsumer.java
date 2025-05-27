747f9cf398d7bcd7a8d67985e6e379b7b1cf5fcc
shubhamsnumbers <shubham@snumbers.com>
2025-05-27 15:18:39 +0530

    added feature to generate report for only one env

--- src/main/java/com/lmg/crawler_qa_tester/service/ReportGeneratorConsumer.java
+++ src/main/java/com/lmg/crawler_qa_tester/service/ReportGeneratorConsumer.java
@@ -1,5 +1,6 @@
 package com.lmg.crawler_qa_tester.service;
 import com.lmg.crawler_qa_tester.constants.LinkStatusEnum;
+import com.lmg.crawler_qa_tester.constants.PageTypeEnum;
 import com.lmg.crawler_qa_tester.constants.ReportStatus;
 import com.lmg.crawler_qa_tester.dto.ReportDetails;
 import com.lmg.crawler_qa_tester.repository.ReportRepository;
@@ -7,6 +8,7 @@ import com.lmg.crawler_qa_tester.repository.entity.CrawlDetailEntity;
 import com.lmg.crawler_qa_tester.repository.entity.CrawlHeaderEntity;
 import com.lmg.crawler_qa_tester.repository.internal.CrawlDetailRepository;
 import com.lmg.crawler_qa_tester.repository.internal.CrawlHeaderRepository;
+import com.lmg.crawler_qa_tester.util.UrlUtil;
 import com.opencsv.CSVWriter;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,18 +54,86 @@ public class ReportGeneratorConsumer {
             {
                 throw new RuntimeException("Crawl Header is empty");
             }
-        String fromURL =    optionalCrawlHeader.get().getCompareFromBaseUrl();
-        String toURL  =      optionalCrawlHeader.get().getCompareToBaseUrl();
-        Map<String,CrawlDetailEntity> fromEnv = new HashMap<>();
-        Map<String,CrawlDetailEntity> toEnv = new HashMap<>();
-
-        for (CrawlDetailEntity detail : crawlDetails) {
-            if (detail.getEnv().equals("FROM_ENV")) {
-                fromEnv.put(detail.getPath(), detail);
-            } else {
-                toEnv.put(detail.getPath(), detail);
+            String toURL = optionalCrawlHeader.get().getCompareToBaseUrl();
+            if(toURL!=null)
+            {
+                reportForCompareEnv(optionalCrawlHeader,crawlDetails,reportDetail,reportId);
             }
+            else
+            {
+                reportForEnv(optionalCrawlHeader,crawlDetails,reportDetail,reportId);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            updateStatus(reportId,ReportStatus.ERROR.getCode());
         }
+    }
+    private void reportForEnv(Optional<CrawlHeaderEntity> optionalCrawlHeader,List<CrawlDetailEntity> crawlDetails,ReportDetails reportDetail, Long reportId)
+    {
+        try {
+            String fromURL = optionalCrawlHeader.get().getCompareFromBaseUrl();
+            String locale = reportDetail.getLocale();
+            String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+            String country = reportDetail.getCountry();
+            URL fromURLObj = new URL(fromURL);
+            String fromURLHost = fromURLObj.getHost();
+            String[] fromURLParts = fromURLHost.split("\\.");
+            String hostName = fromURLParts.length > 2
+                    ? fromURLParts[fromURLParts.length - 2] + "." + fromURLParts[fromURLParts.length - 1]
+                    : fromURLHost;
+            String fromPrefix = fromURLParts.length > 2 ? fromURLParts[0] : "";
+            String fileName = fromPrefix + "_" + hostName + "_" + country + "_" + locale + "_" + dateTime;
+            try (CSVWriter writer = new CSVWriter(new FileWriter(fileLocation + "/" + fileName + ".csv"))) {
+                String[] headerBlock = {"URL " + hostName + "_" + country + "_" + locale,
+                        "Status " ,
+                        "Count " ,
+                       };
+                writer.writeNext(headerBlock);
+                Set<String> uniquePath = new HashSet<>();
+                for (CrawlDetailEntity detail : crawlDetails) {
+                    String path = detail.getPath();
+                    if (uniquePath.contains(path)) {
+                        continue;
+                    }
+                    uniquePath.add(path);
+                    String status = detail.getProcessFlag();
+                    String count="";
+                    PageTypeEnum pageType = UrlUtil.getPageType(path);
+                    if(pageType == PageTypeEnum.CATEGORY || pageType == PageTypeEnum.SEARCH)
+                    {
+                        count = detail.getProductCount()!=null? String.valueOf(detail.getProductCount()):"0";
+                    }
+                    String[] record = {
+                            path,
+                            status,
+                            count
+                    };
+                    writer.writeNext(record);
+                }
+                updateStatus(reportId,ReportStatus.SUCCESS.getCode());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            updateStatus(reportId,ReportStatus.ERROR.getCode());
+
+        }
+
+    }
+    private void reportForCompareEnv(Optional<CrawlHeaderEntity> optionalCrawlHeader,List<CrawlDetailEntity> crawlDetails,ReportDetails reportDetail, Long reportId)
+    {
+        try {
+            String fromURL = optionalCrawlHeader.get().getCompareFromBaseUrl();
+            String toURL = optionalCrawlHeader.get().getCompareToBaseUrl();
+            Map<String, CrawlDetailEntity> fromEnv = new HashMap<>();
+            Map<String, CrawlDetailEntity> toEnv = new HashMap<>();
+            for (CrawlDetailEntity detail : crawlDetails) {
+                if (detail.getEnv().equals("FROM_ENV")) {
+                    fromEnv.put(detail.getPath(), detail);
+                } else {
+                    toEnv.put(detail.getPath(), detail);
+                }
+            }
             String locale = reportDetail.getLocale();
             String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
             String country = reportDetail.getCountry();
@@ -75,47 +145,60 @@ public class ReportGeneratorConsumer {
             String hostName = fromURLParts.length > 2
                     ? fromURLParts[fromURLParts.length - 2] + "." + fromURLParts[fromURLParts.length - 1]
                     : fromURLHost;
-            String fromPrefix= fromURLParts.length > 2 ?fromURLParts[0]:"";
+            String fromPrefix = fromURLParts.length > 2 ? fromURLParts[0] : "";
             String[] toURLParts = toURLHost.split("\\.");
-            String toPrefix = toURLParts.length > 2? toURLParts[0]:"";
-            String fileName = fromPrefix+"_"+toPrefix+"_"+hostName+"_"+country+"_"+locale+"_"+dateTime;
-            try (CSVWriter writer = new CSVWriter(new FileWriter(fileLocation+"/"+fileName+".csv"))) {
-                String[] headerBlock ={ "URL "+hostName+"_"+country+"_"+locale,
-                        "Status "+fromPrefix,
-                        "Status "+toPrefix,
-                        "Count "+fromPrefix,
-                        "Count "+toPrefix,
+            String toPrefix = toURLParts.length > 2 ? toURLParts[0] : "";
+            String fileName = fromPrefix + "_" + toPrefix + "_" + hostName + "_" + country + "_" + locale + "_" + dateTime;
+            try (CSVWriter writer = new CSVWriter(new FileWriter(fileLocation + "/" + fileName + ".csv"))) {
+                String[] headerBlock = {"URL " + hostName + "_" + country + "_" + locale,
+                        "Status " + fromPrefix,
+                        "Status " + toPrefix,
+                        "Count " + fromPrefix,
+                        "Count " + toPrefix,
                         "Count Difference",
                         "Count Difference Percentage"};
                 writer.writeNext(headerBlock);
                 Set<String> uniquePath = new HashSet<>();
-                for(CrawlDetailEntity detail:crawlDetails)
-                {
+                for (CrawlDetailEntity detail : crawlDetails) {
                     String path = detail.getPath();
-                    if(uniquePath.contains(path))
-                    {
+                    if (uniquePath.contains(path)) {
                         continue;
                     }
                     uniquePath.add(path);
-                    String fromEnvStatus = fromEnv.containsKey(path)?fromEnv.get(path).getProcessFlag():LinkStatusEnum.MISSING.getValue();
-                    String toEnvStatus = toEnv.containsKey(path)?toEnv.get(path).getProcessFlag(): LinkStatusEnum.MISSING.getValue();
-                    int countFromEnv = fromEnv.containsKey(path) ? (fromEnv.get(path).getProductCount()!=null? fromEnv.get(path).getProductCount():0): 0;
-                    int countToEnv = toEnv.containsKey(path) ? (toEnv.get(path).getProductCount()!=null? toEnv.get(path).getProductCount():0) : 0;
+                    String fromEnvStatus = fromEnv.containsKey(path) ? fromEnv.get(path).getProcessFlag() : LinkStatusEnum.MISSING.getValue();
+                    String toEnvStatus = toEnv.containsKey(path) ? toEnv.get(path).getProcessFlag() : LinkStatusEnum.MISSING.getValue();
+                    int countFromEnv = fromEnv.containsKey(path) ? (fromEnv.get(path).getProductCount() != null ? fromEnv.get(path).getProductCount() : 0) : 0;
+                    int countToEnv = toEnv.containsKey(path) ? (toEnv.get(path).getProductCount() != null ? toEnv.get(path).getProductCount() : 0) : 0;
                     int countDifference = countFromEnv - countToEnv;
-                    String countPercentage = (countToEnv != 0) ? String.format("%.2f", 100.0 * countDifference / countToEnv) : "NA";
-                    writeDetailsToCsv(path, fromEnvStatus, toEnvStatus, countFromEnv, countToEnv, countDifference, countPercentage, writer);;
+                    String countFrom = "";
+                    String countTo = "";
+                    String countDiff = "";
+                    PageTypeEnum pageType = UrlUtil.getPageType(path);
+                    if(pageType == PageTypeEnum.CATEGORY || pageType == PageTypeEnum.SEARCH)
+                    {
+                        countFrom = String.valueOf(countFromEnv);
+                        countTo=String.valueOf(countToEnv);
+                        countDiff= String.valueOf(countDifference);
+                    }
+                    String countPercentage = (countToEnv != 0) ? String.format("%.2f", 100.0 * countDifference / countToEnv) : "";
+                    writeDetailsToCsv(path, fromEnvStatus, toEnvStatus, countFrom, countTo, countDiff, countPercentage, writer);
                 }
             }
-
-            reportRepository.updateStatusAndTimeById(reportId, ReportStatus.SUCCESS.getCode());
-
-        } catch (Exception e) {
+            updateStatus(reportId,ReportStatus.SUCCESS.getCode());
+            //reportRepository.updateStatusAndTimeById(reportId, ReportStatus.SUCCESS.getCode());
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
-            reportRepository.updateStatusAndTimeById(reportId, ReportStatus.ERROR.getCode());
+            updateStatus(reportId,ReportStatus.ERROR.getCode());
         }
     }
+    private void  updateStatus(Long reportId, String message)
+    {
+        reportRepository.updateStatusAndTimeById(reportId, message);
+    }
     private void writeDetailsToCsv(String path, String fromEnvStatus, String toEnvStatus,
-                                   int countFromEnv, int countToEnv, int countDifference, String countPercentage,
+                                   String countFromEnv, String countToEnv, String countDifference, String countPercentage,
                                    CSVWriter writer) throws IOException {
 
         String[] record = {
